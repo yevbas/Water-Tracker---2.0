41ddd9a589ff7696d200249c569c3c63e378328e
Yevhen Basistyi <psik726@gmail.com>
2025-09-08 14:54:11 +0200

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Jackson  on 08/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
